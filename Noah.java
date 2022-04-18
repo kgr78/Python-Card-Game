4686583977dfbeb40cfb3254017e5e65a74e6427
Michael <mge55@uclive.ac.nz>
2022-04-18 14:38:20 +1200

    Meeting with Kahu

---
+++ Noah.java
@@ -0,0 +1,14 @@
+public class Noah extends Monster {
+	
+	public Noah(int health, int attack1, int attack2, int attack3) {
+		super("Noah", false, "Although one of the oldest monsters on the grid, the fiery monster rages in battle. With a dumbell in one hand and a tennis racket in the other, Noah does not lose many battles just alot tennis matches", health, attack1, attack2, attack3, 100, 80, 50, 200, 100, 40, 10, 50, 20, 80, 50);
+	}
+	
+	
+	public static void main(String[] args) {
+		Monster noah = new Noah(100, 20, 30, 60);
+		System.out.println(noah.getDescription());
+	}
+	
+	
+}
