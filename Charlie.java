c489257e554012a687ae46ba0a0835e598b51028
Michael <mge55@uclive.ac.nz>
2022-05-08 19:59:51 +1200

    Added comments for all classes

--- Charlie.java
+++ Charlie.java
@@ -1,5 +1,10 @@
 public class Charlie extends Monster {
 	
+	
+	/**Charlie class constructor
+	 *
+	 * Initializes the Monsters name, faint status, description, attack1, attack2, attack3, attack1 odds, attack2 odds, attack3 odds, health upper bound, heath lower bound, attack1 upper bound, attack1 lower bound, attack2 upper bound, attack2 lower bound, attack3 upper bound, attack3 lower bound.
+	*/
 	public Charlie(int health, int attack1, int attack2, int attack3) {
 		super("Charlie", false, "Dormant through the day, but awake and ready to fight during the night. This monster is sure to get busy through the night.", health, attack1, attack2, attack3, 90, 70, 40, 240, 140, 45, 20, 60, 35, 85, 60);
 	}
--- Hugo.java
+++ Hugo.java
@@ -1,5 +1,9 @@
 public class Hugo extends Monster 
 {
+	/**Hugo class constructor
+	 *
+	 * Initializes the Monsters name, faint status, description, attack1, attack2, attack3, attack1 odds, attack2 odds, attack3 odds, health upper bound, heath lower bound, attack1 upper bound, attack1 lower bound, attack2 upper bound, attack2 lower bound, attack3 upper bound, attack3 lower bound.
+	*/
 	public Hugo(int health, int attack1, int attack2, int attack3) {
 		super("Hugo", false, "A very calculated Monster. Rarely misses attacks due to the monster's understanding of probability thereom. Also extremely tanky due to diet of chicken jalfrezi.", health, attack1, attack2, attack3, 100, 95, 85, 300, 200, 28, 3, 31, 6, 40, 15);
 	}
