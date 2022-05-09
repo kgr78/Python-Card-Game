4a4ead403875faeeb8a749bc11b50a44d066afe9
Kahu Griffin <kgr78@uclive.ac.nz>
2022-05-09 01:09:22 +0000

    Delete HealthItem.java

--- src/HealthItem.java
+++
@@ -1,7 +0,0 @@
-public class HealthItem extends GettingItems{
-	
-	public HealthItem(int price) {
-		super("Super Health Crystal", 10, false, "A Crystal that heals and recovers one fainted monster", 0, 5, 5, 10); 
-	}
-
-}
